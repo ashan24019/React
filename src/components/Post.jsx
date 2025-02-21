07eb1b272d67d9e044ebd41fbffa31d483d32b00
Ashan <ashanvimodh241@gmail.com>
2025-02-21 08:24:30 +0530

    Outputting dynemic values

---
+++ src/components/Post.jsx
@@ -0,0 +1,14 @@
+const names = ['Ashan', 'Vimodh']
+
+function Post() {
+const chosenName = Math.random() > 0.5 ? names[0] : names[1]
+
+    return (
+        <div>
+            <p>{chosenName}</p>
+            <p>React is really good till now</p>
+        </div>
+    );
+}
+
+export default Post;
