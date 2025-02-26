aca9af6d7b49915d32ef5178f7de2883611c6163
Ashan <ashanvimodh241@gmail.com>
2025-02-26 14:56:19 +0530

    Preparing the app for state management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
+import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostList.module.css';
 
 function PostsList() {
     return (
-        <ul className={classes.posts}>
-            <Post author="Ashan" body = "Hello Harsha" />
-            <Post author="Harsha" body = "Hi Ashan" />
-        </ul>
-    )
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author="Ashan" body = "Hello Harsha" />
+                <Post author="Harsha" body = "Hi Ashan" />
+            </ul>
+        </>
+    );
 }
 
 export default PostsList;
